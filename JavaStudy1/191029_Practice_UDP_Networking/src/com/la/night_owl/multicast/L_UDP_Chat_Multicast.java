c0637716a839f49d3bdfbbf56db39f380e0546fc
Night-Owl-La <fkwjddn@gmail.com>
2019-10-30 11:10:26 +0900

    Rename. Practice Multicasting

--- JavaStudy1/191029_Practice_UDP_Networking/src/com/la/night_owl/multicast/L_UDP_Chat_Multicast.java
+++ JavaStudy1/191029_Practice_UDP_Networking/src/com/la/night_owl/multicast/L_UDP_Chat_Multicast.java
@@ -5,19 +5,24 @@ import java.awt.event.*;
 import java.io.IOException;
 import java.net.*;
 import javax.swing.*;
+import java.util.*;
 
 public class L_UDP_Chat_Multicast extends JFrame {
+	public static final String MULTI_CAST_ADDRESS = "224.0.0.2";
+	public static final int PORT_ID = 6000;
+	
 	JTextArea jta_ChattingDisplay;	// 채팅창
 	JTextField jtf_Address;
 	JTextField jtf_Message;
 	
-	JList<String> jlist_group;
+	JList<String> jlist_group;	// 우측 주소창
+	java.util.List<String> group_List = new ArrayList<String>();
 	
 	Font font = new Font("굴림체", Font.BOLD, 20);
 	
 	String nick_Name = "lll";
 	
-	DatagramSocket client;
+	MulticastSocket client;
 
 	public L_UDP_Chat_Multicast() {
 		super("title");
@@ -31,7 +36,7 @@ public class L_UDP_Chat_Multicast extends JFrame {
 		init_Input();
 		init_GroupList();
 		init_KeyEvent();
-//		init_Socket();
+		init_Socket();
 		
 		this.pack();
 	}
@@ -42,8 +47,8 @@ public class L_UDP_Chat_Multicast extends JFrame {
 		jsp.setPreferredSize(new Dimension(120,0));
 		this.add(jsp, BorderLayout.EAST);
 		
-		String[] join_array = {"244,0,0,1","244,0,0,2","244,0,0,3"};
-		jlist_group.setListData(join_array);
+//		String[] join_array = {"244,0,0,1","244,0,0,2","244,0,0,3"};
+//		jlist_group.setListData(join_array);
 	}
 
 	private void init_Display() {
@@ -59,30 +64,30 @@ public class L_UDP_Chat_Multicast extends JFrame {
 	}
 	private void init_Input() {
 		JPanel p = new JPanel(new GridLayout(3, 1));
-		jtf_Address = new JTextField("255.255.255.255");
+		jtf_Address = new JTextField(MULTI_CAST_ADDRESS);
 		jtf_Message = new JTextField();
 		jtf_Address.setFont(font);
 		jtf_Message.setFont(font);
 		
-		JPanel btn_p = new JPanel(new GridLayout(1,2));
-		JButton jbt_Join = new JButton("가입");
-		JButton jbt_Leave = new JButton("탈퇴");
-		btn_p.add(jbt_Join);
-		btn_p.add(jbt_Leave);
+		JPanel btn_Panel = new JPanel(new GridLayout(1,2));
+		JButton btn_Join = new JButton("가입");
+		JButton btn_Leave = new JButton("탈퇴");
+		btn_Panel.add(btn_Join);
+		btn_Panel.add(btn_Leave);
 		
 		p.add(jtf_Address);
-		p.add(btn_p);
+		p.add(btn_Panel);
 		p.add(jtf_Message);
 		
 		this.add(p, BorderLayout.SOUTH);
 		
-		jbt_Join.addActionListener(new ActionListener() {
+		btn_Join.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				joinTo_Group();
 			}
 		});
-		jbt_Leave.addActionListener(new ActionListener() {
+		btn_Leave.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				leaveFrom_Group();
@@ -92,12 +97,58 @@ public class L_UDP_Chat_Multicast extends JFrame {
 	}
 
 	protected void joinTo_Group() {
-		// TODO Auto-generated method stub
+		String ipAddr = jtf_Address.getText().trim();
+		// 1. 내용이 비어있으면
+		if(ipAddr.isEmpty()) { JOptionPane.showMessageDialog(this, "가입할 주소를 입력하세요."); return; }
+		
+		// 2. 가입 여부 확인.
+		if(group_List.contains(ipAddr)) { JOptionPane.showMessageDialog(this, "이미 가입된 주소임."); return; }
+		
+		try {
+			// 가입 시킴.
+			InetAddress ia = InetAddress.getByName(ipAddr);
+			client.joinGroup(ia);
+			group_List.add(ipAddr);
+			
+			//(EAST)창에 가입목록 출력.
+			String [] group_array = new String[group_List.size()];
+			group_List.toArray(group_array);
+			
+			jlist_group.setListData(group_array);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("실패");
+		}
 		
 	}
 	protected void leaveFrom_Group() {
-		// TODO Auto-generated method stub
+		String ipAddr = jlist_group.getSelectedValue();
+		// 1. 내용이 비어있으면
+		if(ipAddr==null || ipAddr.isEmpty()) { JOptionPane.showMessageDialog(this, "탈퇴할 주소를 입력해주세요."); return; }
+		// 2. 가입 여부 확인.
+		if(! group_List.contains(ipAddr)) { JOptionPane.showMessageDialog(this, "없는 주소임."); return; }
 		
+		int result = JOptionPane.showConfirmDialog(this, "정말 탈퇴합니까?", "그룹탈퇴", JOptionPane.YES_NO_OPTION);
+		if(result==JOptionPane.NO_OPTION) return; 
+		
+		try {
+			// 탈퇴 시킴.
+			InetAddress ia = InetAddress.getByName(ipAddr);
+			client.leaveGroup(ia);
+			group_List.remove(ipAddr);
+			
+			//(EAST)창에 가입목록 출력.
+			String [] group_array = new String[group_List.size()];
+			if(group_array.length == -1) { group_array= new String[0];}	// 널 포인트 익셉션 핸들링.
+			group_List.toArray(group_array);
+			
+			jlist_group.setListData(group_array);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("실패");
+		}
 	}
 
 	private void init_KeyEvent() {
@@ -124,7 +175,7 @@ public class L_UDP_Chat_Multicast extends JFrame {
 			InetAddress ia = InetAddress.getByName(ip);
 			
 			byte [] send_buff = send_Data.getBytes();
-			DatagramPacket dp = new DatagramPacket(send_buff, send_buff.length, ia, 5000);
+			DatagramPacket dp = new DatagramPacket(send_buff, send_buff.length, ia, PORT_ID);
 			client.send(dp);
 			
 		} catch (Exception e) {
@@ -136,9 +187,9 @@ public class L_UDP_Chat_Multicast extends JFrame {
 	}
 	private void init_Socket() {
 		try {
-			client = new DatagramSocket(5000);
+			client = new MulticastSocket(PORT_ID);
 			read_Message();
-		} catch (SocketException e) {
+		} catch (Exception e) {
 			
 			e.printStackTrace();
 		}

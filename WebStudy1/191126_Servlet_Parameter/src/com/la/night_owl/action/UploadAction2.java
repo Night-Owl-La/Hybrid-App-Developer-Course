159fd2a8aac5cd9c4470cba274363d5d5f82442f
unknown <fkwjddn@gmail.com>
2019-11-27 12:13:10 +0900

    modify.

--- WebStudy1/191126_Servlet_Parameter/src/com/la/night_owl/action/UploadAction2.java
+++ WebStudy1/191126_Servlet_Parameter/src/com/la/night_owl/action/UploadAction2.java
@@ -17,46 +17,41 @@ import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 /**
  * Servlet implementation class UploadAction
  */
-@WebServlet("/upload.do")
-public class UploadAction extends HttpServlet {
+@WebServlet("/upload2.do")
+public class UploadAction2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		ServletContext application = request.getServletContext();
-		String web_Path= "/upload/";
+		String web_Path = "/upload/";
 		String saveDir = application.getRealPath(web_Path);
 		int maxSize = 1024 * 1024 * 100;
 
 		MultipartRequest mr = new MultipartRequest(request, saveDir, maxSize, "utf-8", new DefaultFileRenamePolicy());
 		String title = mr.getParameter("title");
-//		System.out.println(title);
 
-		File file;
-		String fileName;
-		String original_FileName = mr.getOriginalFileName("photo");
+		File photo_File_1 = null;
+		File photo_File_2 = null;
+		String fileName = null;
 
 		try {
-			file = mr.getFile("photo");
-			fileName = file.getName();
+			photo_File_1 = mr.getFile("photo1");
+			photo_File_2 = mr.getFile("photo2");
 		} catch (Exception e) {
 			fileName = "파일이 없습니다.";
+			System.out.println(fileName);
+			return;
 		}
-		
+
 		response.setContentType("text/html; charset=utf-8;");
 		PrintWriter out = response.getWriter();
-		
-		out.printf("제목:%s<br>", title);
-		out.printf("<img src='upload/%s' width='200'<br>", fileName);
-		out.printf("<a href='FileUpload_Form.html'>다시하기</a>");
-		
 
-//		System.out.println(fileName);
-		
+		out.printf("제목:%s<br>", title);
+		out.printf("<img src='upload/%s' width='200'<br>", photo_File_1.getName());
+		out.printf("<img src='upload/%s' width='200'<br>", photo_File_2.getName());
+		out.printf("<a href='FileUpload_Form2.html'>다시하기</a>");
+
 	}
 
 }

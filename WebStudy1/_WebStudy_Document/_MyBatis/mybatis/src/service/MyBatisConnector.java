323a73541a956f13986979eecadd84a4c01c6d99
unknown <fkwjddn@gmail.com>
2019-12-17 18:31:21 +0900

    update document

--- WebStudy1/_WebStudy_Document/_MyBatis/mybatis/src/service/MyBatisConnector.java
+++ WebStudy1/_WebStudy_Document/_MyBatis/mybatis/src/service/MyBatisConnector.java
@@ -7,32 +7,30 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-
 public class MyBatisConnector {
-    
-	SqlSessionFactory  factory=null;
-	private static MyBatisConnector connector; 
-	
-	public MyBatisConnector()
-	{
+
+	SqlSessionFactory factory = null;
+	private static MyBatisConnector connector;
+
+	public MyBatisConnector() {
 		try {
-			//sqlMapConfig.xml 읽어들인다.
+			// sqlMapConfig.xml
 			Reader reader = Resources.getResourceAsReader("config/mybatis/sqlMapConfig.xml");
 			factory = new SqlSessionFactoryBuilder().build(reader);
 			reader.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	//single-ton
-	public static MyBatisConnector getInstance(){
-		if(connector==null)
+
+	// single-ton
+	public static MyBatisConnector getInstance() {
+		if (connector == null)
 			connector = new MyBatisConnector();
 		return connector;
 	}
-	public SqlSessionFactory  getSqlSessionFactory()
-	{
+
+	public SqlSessionFactory getSqlSessionFactory() {
 		return factory;
 	}
 }

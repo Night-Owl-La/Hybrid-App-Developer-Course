18b9977e1acac80b9c1fe07b969b3601c86b5697
JWL <fkwjddn@gmail.com>
2020-01-09 12:42:34 +0900

    DB Test계정 sqlplus 글로벌 옵션 설정파일 추가

---
+++ WebStudy1/_Global_SQL/_test-account_init_config.sql
@@ -0,0 +1,18 @@
+-- member table config.
+set linesize 1000;
+col name format a30;
+col id format a30;
+col pwd format a30;
+col zipcode format a30;
+col address format a30;
+col ip format a20;
+col grade format a30;
+col modifydate format a20;
+
+-- board table config.
+col board_title format a30;
+col board_content format a30;
+col board_ip format a20;
+col board_regdate format a20;
+col board_use_yn format a20;
+col user_name format a15;
